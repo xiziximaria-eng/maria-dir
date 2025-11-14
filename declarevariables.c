220d93aa6933c2f529407c74d0b80e6a8fb232e1
Maria <xiziximaria@gmail.com>
2025-11-14 09:33:09 +0800

    declare variables

---
+++ declarevariables.c
@@ -0,0 +1,16 @@
+//ISIDORO, MA. CHRISTINA DHINE P. DIT 1-2
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("DECLARING VARIABLES\n");
+    // Declare variables integer
+    int a = 1998;
+    // Declare variable float
+    float b = 16.7;
+    // Declare variable character
+    char c = 'b';
+    printf("Integer: %d,\n Float: %f,\n Character: %c\n", a, b, c);
+    return 0;
+}
